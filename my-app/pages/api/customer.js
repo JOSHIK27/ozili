67a23607466e8b1f15d241039fd9959b9e865e9a
JOSHIK <joshikroshan4021@gmail.com>
2024-02-09 20:46:46 +0000

    customer form added

---
+++ my-app/pages/api/customer.js
@@ -0,0 +1,50 @@
+import { supabase } from "../../db/supabase";
+
+export default async function handler(req, res) {
+  if (req.method == "POST") {
+    const body = JSON.parse(req.body);
+    console.log(body);
+    const { data, error } = await supabase.from("customertbl").insert({
+      nickname: body.nickname,
+      customerfullname: body.customerfullname,
+      primaryno: body.primaryno,
+      secondaryno: body.secondaryno,
+      source: body.source,
+      referral: body.ifReferred,
+      profession: body.profession,
+      group: body.group,
+      addressline1: body.addressline1,
+      addressline2: body.addressline2,
+      addressline3: body.addressline3,
+      city: body.city,
+      state: body.state,
+      pincode: body.pincode,
+      emailid: body.email,
+    });
+    console.log(error);
+    res.json(["success"]);
+  }
+  if (req.method == "PUT") {
+    const body = JSON.parse(req.body);
+    console.log(body);
+    const { data, error } = await supabase.from("customertbl").update({
+      nickname: body.nickname,
+      customerfullname: body.customerfullname,
+      primaryno: body.primaryno,
+      secondaryno: body.secondaryno,
+      source: body.source,
+      referral: body.ifReferred,
+      profession: body.profession,
+      group: body.group,
+      addressline1: body.addressline1,
+      addressline2: body.addressline2,
+      addressline3: body.addressline3,
+      city: body.city,
+      state: body.state,
+      pincode: body.pincode,
+      emailid: body.email,
+    });
+    console.log(error);
+    res.json(["success"]);
+  }
+}
